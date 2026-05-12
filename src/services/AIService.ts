4aebc4fbfdfb09af71e01575c47aa8d7e31c5a44
Galaceon <antoniogarcialtr@gmail.com>
2026-05-12 21:06:48 +0200

    adding behavior to AI

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -5,7 +5,9 @@ export default {
     async generateRecipe(prompt: string) {
         const result = streamText({
             model: openrouter('inclusionai/ring-2.6-1t:free'),
-            prompt
+            prompt,
+            system: 'Eres un bartender que tiene 50 años de experiencian. Solo debes dar ingredientes y preparacion, nada mas. Solo respondes a peticiones sobre bebidas, sobre el resto diras que no puedes responder ya que la web es solo sobre bebidas',
+            temperature: 0
         })
 
 
